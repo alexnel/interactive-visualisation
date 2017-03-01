26fe6de7df2fcf8e916fdd309b3e67c2764e4893
Alexandra Nel <alexandranel@gmail.com>
2017-03-01 10:34:53 +0200

    Command line arguments added.

--- runIV.sh
+++ runIV.sh
@@ -1,4 +1,7 @@
-fdp -Tplain -O graphtest.dot
-fdp -Tpng -O graphtest.dot
+format=$2
+file=$2
+
+$1 -Tplain -O $2
+$1 -Tpng -O $2
 stack build
-stack exec interactive-visualisation
+stack exec interactive-visualisation $2
--- src/Main.hs
+++ src/Main.hs
@@ -9,18 +9,17 @@ import Control.Monad (unless, when)
 import qualified Graphics.Rendering.OpenGL as GL
 import qualified Graphics.UI.GLFW as G
 import System.Exit
+import System.Environment
 import System.IO
 import Data.Char
 import Data.List
-import Data.List.Split
+import qualified Data.ByteString as B 
+import qualified Data.ByteString.Char8 as C 
 import Data.IORef
 import System.IO.Unsafe 
 import qualified Data.Map as Map
-import Control.Monad
 import Graphics.Gloss.Rendering
-import Graphics.Gloss.Data.Color 
-import Graphics.Gloss.Data.Picture
-import Control.Concurrent (threadDelay)
+--import Control.Concurrent (threadDelay)
 import qualified Graphics.UI.GLUT as GLUT
 import Data.GraphViz (
   GraphvizParams,
@@ -85,20 +84,19 @@ maybe' m nothingRes f = case m of
 
 -- Callback operations --
 
-errorCallback :: G.ErrorCallback
-errorCallback err description = hPutStrLn stderr description
+--errorCallback :: G.ErrorCallback
+--errorCallback err description = hPutStrLn stderr description
 
 
 keyCallback :: G.KeyCallback
-keyCallback window key scancode action mods
+keyCallback window key _ action _
   | key == G.Key'Escape && action == G.KeyState'Pressed = G.setWindowShouldClose window True
-  | key == G.Key'Space && action == G.KeyState'Pressed = printkey "space"
+  | key == G.Key'Space && action == G.KeyState'Pressed = putStrLn "space"
   | otherwise = G.setWindowShouldClose window False
-  where printkey x = putStrLn x
 
 
 charCallback :: G.CharCallback
-charCallback window codepoint = do
+charCallback _ codepoint = do
   putChar codepoint
   putStr "\n"
 
@@ -114,12 +112,12 @@ charCallback window codepoint = do
 
 
 mouseButtonCallback :: G.MouseButtonCallback
-mouseButtonCallback window button action mods= do
+mouseButtonCallback window button action _ = do
   (xpos, ypos) <- G.getCursorPos window 
   (nameFind,_) <- isNode xpos ypos
   --(connectionsRes, name) <- connections nameFind
   --when (button == G.MouseButton'1 && action == G.MouseButtonState'Pressed && result /= []) $ print result
-  when (button == G.MouseButton'1 && action == G.MouseButtonState'Pressed && nameFind /= "") $ print ("You clicked node " ++ nameFind)
+  when (button == G.MouseButton'1 && action == G.MouseButtonState'Pressed && nameFind /= B.empty) $ putStrLn ("You clicked node " ++ (C.unpack nameFind))
 
 
 ----calculate the number of conections a node has
@@ -151,62 +149,77 @@ mouseButtonCallback window button action mods= do
 -- gnome classes, graphviz functionality --
 -- not working --
 
---gnomes :: Gr Text Text
---gnomes = mkGraph [(1, pack "Hello"), (3, pack "World")] [(1, 3, pack "?")]
+gnomes :: Gr Text Text
+gnomes = mkGraph [(1, pack "Hello"), (3, pack "World")] [(1, 3, pack "?")]
 
 
---gnomeParams :: GraphvizParams n Text Text () Text
---gnomeParams = nonClusteredParams {
---  globalAttributes = ga,
---  fmtNode = fn,
---  fmtEdge = fe
---  }
---  where
---    ga = [
---      GraphAttrs [
---         RankDir FromLeft,
---         BgColor [toWColor Transparent]
---         ],
---      NodeAttrs [
---        Shape BoxShape,
---        FillColor [toWColor White],
---        Style [SItem Filled []]
---        ]
---      ]
+gnomeParams :: GraphvizParams n Text Text () Text
+gnomeParams = nonClusteredParams {
+  globalAttributes = ga,
+  fmtNode = fn,
+  fmtEdge = fe
+  }
+  where
+    ga = [
+      GraphAttrs [
+         RankDir FromLeft,
+         BgColor [toWColor Transparent]
+         ],
+      NodeAttrs [
+        Shape BoxShape,
+        FillColor [toWColor White],
+        Style [SItem Filled []]
+        ]
+      ]
 
---    fn (n,l) = [(Label . StrLabel) l]
---    fe (f,t,l) = [(Label . StrLabel) l]
+    fn (_,l) = [(Label . StrLabel) l]
+    fe (_,_,l) = [(Label . StrLabel) l]
 
 
 scalingFactor :: Float
-scalingFactor = 60.0
+scalingFactor = 100.0
 
 
 ab :: IORef (Map.Map Float Float)
 ab = unsafePerformIO $ newIORef Map.empty
 
 
-nodes :: IORef (Map.Map String (Float,Float))
+nodes :: IORef (Map.Map B.ByteString (Float,Float))
 nodes = unsafePerformIO $ newIORef Map.empty
 
 
-edges :: IORef (Map.Map String String)
+edges :: IORef (Map.Map B.ByteString B.ByteString)
 edges = unsafePerformIO $ newIORef Map.empty
+
+
+extractFirst :: [String] -> String
+extractFirst (x:[]) = x
+extractFirst (x:_) = x
 
 
 main :: IO ()
 main = do 
+
   --putStrLn "Insert filename, where filename.dot , filename.dot.plain and filename.dot.png exists: "
   --filenameRead <- getLine
-  let filenameRead = "graphtest"
-  let filenamePlain = filenameRead ++ ".dot.plain"
-  let filenamePNG = filenameRead ++ ".dot.png"
+  args <- getArgs
+  let filenameRead = extractFirst args
+  let filenamePlain = filenameRead ++ ".plain"
+  let filenamePNG = filenameRead ++ ".png"
+
+
+  --graphviz stuff
+  --not working
+  putStr $ unpack $ renderDot $ toDot $ graphToDot gnomeParams gnomes
+  doDots [ ("test.png" , graphToDot gnomeParams gnomes) ]
+
+
 
   --initialisations
   readfile filenamePlain
   GLUT.exit
   (_,_) <- GLUT.getArgsAndInitialize
-  G.setErrorCallback (Just errorCallback)
+  --G.setErrorCallback (Just errorCallback)
   successfulInit <- G.init
   glossState <- initState
 
@@ -227,25 +240,22 @@ main = do
           G.terminate
           exitSuccess
 
-  ----graphviz stuff
-  ----not working
-  --putStr $ unpack $ renderDot $ toDot $ graphToDot gnomeParams gnomes
-  --doDots [ ("test.png" , graphToDot gnomeParams gnomes) ]
+
 
 
 mainLoop :: G.Window -> State -> String -> IO ()
 mainLoop w glossState filenamePNG = unless' (G.windowShouldClose w) $ do
-    (width, height) <- G.getFramebufferSize w
-    let ratio = fromIntegral width / fromIntegral height
-    GL.viewport GL.$= (GL.Position 0 0, GL.Size (fromIntegral width) (fromIntegral height))
-    --GL.clear [GL.ColorBuffer]
+    --(width, height) <- G.getFramebufferSize w
+    --let ratio = fromIntegral width / fromIntegral height
+    --GL.viewport GL.$= (GL.Position 0 0, GL.Size (fromIntegral width) (fromIntegral height))
+    ----GL.clear [GL.ColorBuffer]
     
-    GL.matrixMode GL.$= GL.Projection
-    GL.loadIdentity
-    GL.ortho (negate ratio) ratio (negate 1.0) 1.0 1.0 (negate 1.0)
-    GL.matrixMode GL.$= GL.Modelview 0
+    --GL.matrixMode GL.$= GL.Projection
+    --GL.loadIdentity
+    --GL.ortho (negate ratio) ratio (negate 1.0) 1.0 1.0 (negate 1.0)
+    --GL.matrixMode GL.$= GL.Modelview 0
     
-    GL.loadIdentity
+    --GL.loadIdentity
 
     loadJuicyPNG filenamePNG >>= maybe (putStrLn "Couldn't load file") displayPic 
 
@@ -258,14 +268,14 @@ mainLoop w glossState filenamePNG = unless' (G.windowShouldClose w) $ do
 readfile :: FilePath -> IO ()
 readfile x = do
   handle <- openFile x ReadMode
-  contents <- hGetContents handle
-  let dotplain = lines contents
+  contents <- B.hGetContents handle
+  let dotplain = C.splitWith (=='\n') contents
   store dotplain
-  hClose handle 
+  --B.hClose handle 
 
 
 --iterate through list and save data
-store :: [String] -> IO ()
+store :: [B.ByteString] -> IO ()
 store [] = putStrLn "done storing nodes"
 store [_] = putStrLn "done storing nodes"
 store (x:xs) = do
@@ -274,27 +284,27 @@ store (x:xs) = do
 
 
 --test for what information the line is giving and write to file
-save :: String -> IO ()
+save :: B.ByteString -> IO ()
 save x = do
-  let allLines = splitOn " " x
-  if "graph" `elem` allLines 
+  let allLines = C.split ' ' x
+  if (C.pack "graph") `elem` allLines 
     then initialiseFile (allLines!!2) (allLines!!3)
-    else if "node" `elem` allLines 
+    else if (C.pack "node") `elem` allLines 
       then addNode (allLines!!1) (allLines!!2) (allLines!!3)
-      else if "edge" `elem` allLines
+      else if (C.pack "edge") `elem` allLines
         then addConnection (allLines!!1) (allLines!!2)
         else putStrLn "done"
 
 
 --input the original size of the image from the .plain file--
-initialiseFile :: [Char] -> [Char] -> IO ()
+initialiseFile :: B.ByteString -> B.ByteString -> IO ()
 initialiseFile a b = do
-  let aFloat = if (checkNumber a)
-          then (read a) :: Float
+  let aFloat = if (checkNumber (C.unpack a))
+          then (read (C.unpack a)) :: Float
           else 
             0
-  let bFloat = if (checkNumber b) 
-            then (read b) :: Float
+  let bFloat = if (checkNumber (C.unpack b)) 
+            then (read (C.unpack b)) :: Float
             else 
               0
   atomicModifyIORef ab (\m -> (Map.insert aFloat bFloat m, ()))
@@ -302,15 +312,16 @@ initialiseFile a b = do
 
 
 --add a node information to the pos.txt file--
-addNode :: [Char] -> String -> String -> IO ()
+addNode :: B.ByteString -> B.ByteString -> B.ByteString -> IO ()
 addNode name c d = do
-  [(a,b)] <- getAB
-  let cint = if (checkNumber c) 
-              then (read c) :: Float
+  [(_,b)] <- getAB
+
+  let cint = if (checkNumber (C.unpack c)) 
+              then (read (C.unpack c)) :: Float
               else 
                 0
-  let dint = if (checkNumber d)
-              then (read d) :: Float
+  let dint = if (checkNumber (C.unpack d))
+              then (read (C.unpack d)) :: Float
               else
                 0
   let xpos = (cint*scalingFactor)
@@ -319,7 +330,7 @@ addNode name c d = do
   --appendFile "pos.txt" ("node " ++ name ++ " " ++ show xpos ++ " " ++ show ypos ++ "\n")
 
 --check for if it is a double or int--
-checkNumber :: [Char] -> Bool 
+checkNumber :: String -> Bool 
 checkNumber [] = True 
 checkNumber (x:xs) = (isDigit x || x == '.') && checkNumber xs 
 
@@ -333,7 +344,7 @@ getAB = do
 
 
 --add edges to the pos.txt file--
-addConnection :: [Char] -> [Char] -> IO ()
+addConnection :: B.ByteString -> B.ByteString -> IO ()
 addConnection name1 name2 = do
   atomicModifyIORef edges (\m -> (Map.insert name1 name2 m, ()))
   --appendFile "pos.txt" (name1 ++ " " ++ name2 ++ "\n")
@@ -349,7 +360,7 @@ displayPic _ = error "only the Bitmap constructor should be used here"
 
 
 --check if the cursor position is sitting on a node--
-isNode :: Double -> Double -> IO (String,(Float,Float))
+isNode :: Double -> Double -> IO (B.ByteString,(Float,Float))
 isNode xpos ypos = do
   --contents <- readFileStrict "pos.txt"
   --let dotplain = lines contents
@@ -367,7 +378,7 @@ isNode xpos ypos = do
   let index = elemIndex True nodesBool
 
   if index == Nothing
-    then return ("",(0.0,0.0))
+    then return (B.empty,(0.0,0.0))
     else return (dotList!!(extractMaybe index))
 
 
@@ -376,7 +387,7 @@ extractMaybe (Just x) = x
 
 
 --check if cursor position is within curtain range to node--
-inRange :: Double -> Double -> (String,(Float,Float)) -> Bool
+inRange :: Double -> Double -> (B.ByteString,(Float,Float)) -> Bool
 inRange xpos ypos (_,(x,y))
   | abs(xint - xpos) < 20 && abs(yint-ypos) < 20 = True
   | otherwise = False
@@ -385,10 +396,10 @@ inRange xpos ypos (_,(x,y))
     yint = realToFrac y :: Double
 
 
---strict file reading
-hGetContentsStrict :: Handle -> IO String
-hGetContentsStrict h = hGetContents h >>= \s -> length s `seq` return s
+----strict file reading
+--hGetContentsStrict :: Handle -> IO B.ByteString
+--hGetContentsStrict h = hGetContents h >>= \s -> B.length s `seq` return s
 
 
-readFileStrict :: FilePath -> IO String
-readFileStrict name =  openFile name ReadMode >>= hGetContentsStrict
+--readFileStrict :: FilePath -> IO B.ByteString
+--readFileStrict name =  openFile name ReadMode >>= hGetContentsStrict
